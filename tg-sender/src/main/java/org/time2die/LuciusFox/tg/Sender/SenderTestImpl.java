8e7f2c001be354958411702393d679980787f6eb
Alexandr Karpov <AVKarpov@bssys.com>
2016-02-01 10:42:52 +0300

    redis-example

--- tg-sender/src/main/java/org/time2die/LuciusFox/tg/Sender/SenderTestImpl.java
+++ tg-sender/src/main/java/org/time2die/LuciusFox/tg/Sender/SenderTestImpl.java
@@ -1,38 +1,51 @@
 package org.time2die.LuciusFox.tg.Sender;
 
-import org.apache.commons.httpclient.methods.GetMethod;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Scanner;
+import org.eclipse.jetty.client.HttpClient;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.*;
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * @author Aleksandr Karpov AV.Karpov@bssys.com
  */
 public class SenderTestImpl implements tgSender {
 
-    String apiUrl = "https://api.telegram.org//bot";
-    String apiKey = "168867787:AAFlmoROO6Sw52ueFaQMaeiirZL5CJLxNEQ";
-    String method = "sendMessage";
-    String testId = "69711013";
-    String url = apiUrl + apiKey + "//" + method + '?'+ "chat_id="+ testId+ "&text=" ;
+    static String apiUrl = "https://api.telegram.org/bot";
+    static String apiKey = "168867787:AAFlmoROO6Sw52ueFaQMaeiirZL5CJLxNEQ";
+    static String method = "sendMessage";
+    static String testId = "69711013";
+    static String url = apiUrl + apiKey + "/" + method + '?' + "chat_id=" + testId + "&text=";
 
     @Override
     public void sendText(String id, String text) {
-        System.out.println(url+text);
-        GetMethod get = new GetMethod(url+text);
+        h2(text);
+    }
+
+
+    static void h2(String text){
+        BufferedReader in = null ;
         try {
-            InputStream in = get.getResponseBodyAsStream();
-            if(in != null) {
-                Scanner sc = new Scanner(in);
-                System.out.println(sc.nextLine());
+            String httpsURL = url+text ;
+            URL myurl = new URL(httpsURL);
+            HttpsURLConnection con = (HttpsURLConnection)myurl.openConnection();
+            InputStream ins = con.getInputStream();
+            InputStreamReader isr = new InputStreamReader(ins);
+            in = new BufferedReader(isr);
+
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null)
+            {
+                System.out.println(inputLine);
             }
+            in.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        get.releaseConnection();
     }
-
-// https://api.telegram.org/bot168867787:AAFlmoROO6Sw52ueFaQMaeiirZL5CJLxNEQ/sendMessage?chat_id=69711013&text=
 }
